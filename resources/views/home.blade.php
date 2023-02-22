48d11c75b5fc3c1f0d406e242f20560cf757af1c
annie-rojas <annie.rojas.serna@gmail.com>
2023-02-21 19:04:32 -0500

    banner del home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,20 @@
 <x-layouts.general>
-<span class="text-white">Aqui va tu contenido</span>
+    <!-- Swiper -->
+    <div class="relative w-full h-[40vh]">
+        <div class="swiper swiper-home w-full h-full">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 1</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 2</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 3</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 4</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 5</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 6</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 7</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 8</div>
+                <div class="swiper-slide text-center text-base bg-white flex justify-center items-center">Slide 9</div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
 </x-layouts.general>
