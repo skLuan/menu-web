b84902770731782495b24d0eae92420b35b78311
Luan E <luan.erazo@skyrocketlab.space>
2023-03-14 11:01:54 -0500

    arreglo de redireccion sidebar y animaciones

--- resources/views/components/nav/tab-bar.blade.php
+++ resources/views/components/nav/tab-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full fixed bottom-0 z-10">
 
-    <div class="relative flex flex-row bg-black-true border-t border-red-navigation border-opacity-20 px-5 z-10 py-2 left-0 bottom-0">
+    <div class="relative flex flex-row bg-black-true border-t border-red-navigation border-opacity-20 px-5 z-20 py-2 left-0 bottom-0">
         <iconify-icon id="menuTab" class="text-white cursor-pointer" icon="material-symbols:menu-rounded" width="45" height="45">
         </iconify-icon>
         <picture id="close-wrapper" class="my-auto ml-auto cursor-pointer">
